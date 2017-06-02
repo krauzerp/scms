af17009f46941154df01bf7439aab0b2b4596563
Krauzer Peter <kp@microlab.at>
2017-06-02 13:07:57 +0200

    V00.50

--- backend/index.php
+++ backend/index.php
@@ -1,13 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>SCMC - 0.0</title>
+  <title>leanCMS</title>
   <!-- Bootstrap -->
-  <link href="css/bootstrap.css" rel="stylesheet">
-  <link href="css/pen.css" rel="stylesheet">
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -15,57 +13,35 @@
   <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
   <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
   <![endif]-->
-  <style>
-  .content:hover{outline: grey dotted 1px;}
-  .content:focus{outline: lightblue auto 5px;}
-  </style>
 </head>
 <body>
   <div class="container">
     <div class="row">
-      <div class="col-md-3">
-      <?php include('core/backend/load_sitemap.php'); ?>
+      <div class="col-md-12">
+        <h1>leanCMS</h1>
       </div>
-      <div class="col-md-9">
+    </div>
+    <div class="row">
+      <div class="col-md-3">
         <!-- Nav tabs -->
         <ul class="nav nav-tabs">
-          <li class="active"><a href="#page_content" data-toggle="tab">Content</a></li>
-          <li><a href="#page_meta" data-toggle="tab">Meta</a></li>
-          <li><a href="#page_publish" data-toggle="tab">Publish</a></li>
+          <li class="active"><a href="#home" data-toggle="tab">Pages</a></li>
+          <li><a href="#objects" data-toggle="tab">Objects</a></li>
+          <li><a href="#settings" data-toggle="tab">Settings</a></li>
         </ul>
         <!-- Tab panes -->
         <div class="tab-content">
-          <div class="tab-pane active" id="page_content">...</div>
-          <div class="tab-pane" id="page_meta">...</div>
-          <div class="tab-pane" id="page_publish">...</div>
+          <div class="tab-pane active" id="home"><?php include 'pages.php'?></div>
+          <div class="tab-pane" id="objects">objects...</div>
+          <div class="tab-pane" id="settings">settings...</div>
         </div>
       </div>
-    </div>
-    <div id="DBG"></div>
-    <!-- Modal -->
-<div id="modalImage" class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-        <h4 class="modal-title" id="myModalLabel">Image</h4>
+      <div class="col-md-9">
+        <p>Content</p>
       </div>
-      <div class="modal-body" id="currentImage">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
   </div>
-</div>
-  </div><!-- /.container-collapse -->
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
   <!-- Include all compiled plugins (below), or include individual files as needed -->
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/pen.js"></script>
-  <script src="js/scmscore.js"></script>
+  <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 </body>
-</html>
